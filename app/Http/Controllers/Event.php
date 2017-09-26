4cacb6a89df3bc8909c3d562cecd7e1bf0cc7a0c
Ryan Rosiek <ryan.rosiek@gmail.com>
2017-09-26 13:21:19 -0400

    fixing event edit bug

--- app/Http/Controllers/Event.php
+++ app/Http/Controllers/Event.php
@@ -58,12 +58,12 @@ class Event extends Controller
         ]);
 
         $event = $this->storeOrUpdateEvent(collect($attributes), new Model());
-  
+
         return redirect()
             ->route('events.index')
             ->with('successMsg', sprintf('Event %s has been successfully created.', $event->title));
     }
- 
+
     /**
      * @param  \App\Models\Event $event
      * @return \Illuminate\Http\Response
@@ -71,7 +71,7 @@ class Event extends Controller
     public function edit(Model $event)
     {
         $end_time = $event->end_date ? $event->end_date->format('H:i') : '';
- 
+
         return view('admin.events.edit', [
             'title' => 'Modify Event',
             'event' => $event,
@@ -79,21 +79,35 @@ class Event extends Controller
             'end_time' => $end_time,
         ]);
     }
- 
+
     /**
-     * @param  \App\Http\Requests\StoreOrUpdateEvent $request
      * @param  \App\Models\Event $event
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function update(StoreOrUpdateEvent $request, Model $event)
+    public function update(Request $request, Model $event)
     {
-        $event = $this->storeOrUpdateEvent($request, $event);
- 
+        $attributes = $request->validate([
+            'title' => ['required', 'max:255'],
+            'detail' => ['max:255'],
+            'start_date' => ['required', 'date_format:n/j/Y'],
+            'start_time' => ['max:5'],
+            'end_date' => ['nullable', 'date_format:n/j/Y', 'after_or_equal:start_date'],
+            'end_time' => ['max:5'],
+            'frequency' => ['nullable', 'in:DAILY,WEEKLY,MONTHLY'],
+            'by_day' => ['nullable', 'in:' . implode(",", array_keys(config('custom.days')))],
+            'interval' => ['nullable', 'integer'],
+            'set_position' => ['nullable', 'integer'],
+            'until' => ['required_with:frequency'],
+        ]);
+
+        $event = $this->storeOrUpdateEvent(collect($attributes), $event);
+
         return redirect()
             ->route('events.index')
             ->with('successMsg', sprintf('Event %s has been successfully updated.', $event->title));
     }
- 
+
     /**
      * @param  \App\Models\Event $event
      * @return \Illuminate\Http\Response
@@ -101,12 +115,12 @@ class Event extends Controller
     public function destroy(Model $event)
     {
         $event->delete();
- 
+
         return redirect()
             ->route('events.index')
             ->with('successMsg', sprintf('Event %s has been deleted.', $event->title));
     }
- 
+
     /**
      * @param  \Illuminate\Support\Collection $attrs
      * @param  \App\Models\Event $model
@@ -117,13 +131,13 @@ class Event extends Controller
         $model->title = $attrs->get('title');
         $model->detail = $attrs->get('detail');
         $model->start_date = new Carbon($attrs->get('start_date') . ' ' . $attrs->get('start_time'));
- 
+
         if (!$attrs->get('start_time'))
             $model->all_day = true;
-   
+
         if ($attrs->get('end_date'))
             $model->end_date = new Carbon($attrs->get('end_date') . ' ' . $attrs->get('end_time'));
- 
+
         if ($attrs->get('frequency')) {
             $model->frequency = $attrs->get('frequency');
             $model->interval = $attrs->get('interval') ?: 1;
@@ -131,9 +145,9 @@ class Event extends Controller
             $model->by_set_pos = $attrs->get('by_set_pos') ?: null;
             $model->until = new Carbon($attrs->get('until'));
         }
-        
+
         $model->save();
- 
+
         return $model;
     }
 }
